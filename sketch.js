e038e5aca9e6425e6676be1d86638da425814982
erixerns <erixerns@gmail.com>
2018-07-07 15:53:16 +0530

    Added Option to Make prims with any point/ Comment code properly

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
 
+// Global Stack
 var stack=[];
+
+// Array to store all points
 var points=[];
+
+// Adjecency matrix
 var matrix;
-// var visited=new Array(false, false, false, false, false);
+
+// Lock Varible so that other points do not hide the cursor from hand
+// i.e in object Point, if mouse in in the circle it shows a HAND
+// however, if mouse is inside '0' it is outside '5' and this '5'
+// changes the cursor from HAND to ARROW which is not what we want.
+// With this variable we lock so that cursor changes only when mouse
+// leaves '0' and it is unaffected by '5'
+var curLock;
+
+// Initilizing prims table
 var primsDistance = new Array(Infinity, Infinity, Infinity, Infinity, Infinity, Infinity);
 var neighbour = new Array(0, 0, 0, 0, 0, 0);
 
+
+// Helper function to create a 2D array easily
 function create2DArray(numRows, numColumns) {
 	let array = new Array(numRows); 
 	for(let i = 0; i < numColumns; i++) {
@@ -14,36 +30,16 @@ function create2DArray(numRows, numColumns) {
 	return array; 
 }
 
-function setup(){
-  // creating adjecency matrix by random
-  matrix=create2DArray(6, 6);
+// Function to make prims table
+function calculatePrims(value, points){
+  // Re-initialize prims table
+  primsDistance=[Infinity, Infinity, Infinity, Infinity, Infinity, Infinity];
+  neighbour=[value, value, value, value, value, value];
+  points.forEach((point)=>{
+    point.visited=false;
+  });
 
-  for(let i=5; i>=0; i--){
-    for(let j=0; j<=i; j++){
-      if(i===j)
-        matrix[i][j]=0;
-      else
-        matrix[i][j]=random()>0.4?1:0;
-      matrix[j][i]=matrix[i][j];
-    }
-  }
-
-  createCanvas(1000, 600);
-  for(i=0; i<6; i++)
-    points[i]=new Point(random(50,550), random(50, 550),i, matrix);
-  
-  // Fill matrix with distances instead of 0 or 1
-  for(let i=5; i>=0; i--){
-    for(let j=0; j<=i; j++){
-      if(matrix[i][j]){
-        matrix[i][j]=Math.sqrt(sq(points[i].x-points[j].x)+sq(points[i].y-points[j].y));
-        matrix[j][i]=matrix[i][j];
-      }
-    }
-  }
-
-  // Prims algorithm
-  stack.push(0);
+  stack.push(value);
   while(stack.length>0){
     let node = stack.pop();
     points[node].visited=true;
@@ -59,7 +55,45 @@ function setup(){
   }
 }
 
+// ==============
+// SETUP Function
+// ==============
+function setup(){
+  createCanvas(1000, 600);
 
+  // creating adjecency matrix by random
+  matrix=create2DArray(6, 6);
+  for(let i=5; i>=0; i--){
+    for(let j=0; j<=i; j++){
+      if(i===j)
+        matrix[i][j]=0;
+      else
+        matrix[i][j]=random()>0.4?1:0;
+      matrix[j][i]=matrix[i][j];
+    }
+  }
+
+  // Make random Points
+  for(i=0; i<6; i++)
+    points[i]=new Point(random(50,550), random(50, 550),i, matrix);
+  
+  // Fill matrix with distances instead of 0 or 1 wrt the points
+  for(let i=5; i>=0; i--){
+    for(let j=0; j<=i; j++){
+      if(matrix[i][j]){
+        matrix[i][j]=Math.sqrt(sq(points[i].x-points[j].x)+sq(points[i].y-points[j].y));
+        matrix[j][i]=matrix[i][j];
+      }
+    }
+  }
+
+  // Prims algorithm for first time wrt '0'
+  calculatePrims(0, points);
+}
+
+// =============
+// DRAW Function
+// =============
 function draw(){
   background(25);
   drawLines(matrix, neighbour);
@@ -68,12 +102,14 @@ function draw(){
   primsTable();
 }
 
+// Function to draw lines between adjacent points
 function drawLines(matrix){
   push();
   stroke(255);
   for(let i=5; i>=0; i--)
     for(let j=0; j<i; j++){
       if(matrix[i][j]){
+        // If the point is a prims neighbour, color it red else white
         if(neighbour[i]===j || neighbour[j]===i)
           stroke(255,0,0);
         else
@@ -84,7 +120,7 @@ function drawLines(matrix){
   pop();
 }
 
-
+// Display information
 function primsTable(){
   push();
   fill(255);
@@ -132,6 +168,9 @@ function primsTable(){
   pop();
 }
 
+// ============
+// Object Point
+// ============
 var Point = function(x, y, i){
   this.i=i;
   this.visited=false;
@@ -142,6 +181,7 @@ var Point = function(x, y, i){
   this.neighbours=[];
 
   this.render=function(){
+    this.checkClick();
     push();
     noStroke();
     fill(this.rgb[0], this.rgb[1], this.rgb[2]);
@@ -154,4 +194,18 @@ var Point = function(x, y, i){
     text(this.i, this.x-4, this.y+4);
     pop();
   };
+
+  this.checkClick=function(){
+    if(Math.abs(mouseX-this.x)<15 && Math.abs(mouseY-this.y)<15){
+      cursor(HAND);
+      curLock=this.i;
+      if(mouseIsPressed){
+        console.log(this.i);
+        calculatePrims(this.i, points);
+      }
+    }
+    else
+      if(curLock===this.i)
+        cursor(ARROW);
+  }
 };
